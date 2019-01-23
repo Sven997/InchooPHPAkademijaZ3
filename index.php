f73b89d2fa992adea1f8fe5734c41236ac2ece71
Sven Bassi <svencro@gmail.com>
2019-01-23 14:30:09 +0100

    first commit funkcija

---
+++ index.php
@@ -0,0 +1,59 @@
+<!
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Zadaca03</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<form action="index.php" method="POST">
+  Broj redaka :  <input type="number" name="redak"><br>
+  Broj stupaca : <input type="number" name="stupac"><br>
+
+    <input type="submit" name="potvrdi">
+
+
+
+</form>
+
+<?php
+
+function  nizMat($x, $y, &$a)
+{
+    $value = 1;
+
+    $rowS = 0;
+    $colS = 0;
+    while($rowS < $x && $colS < $y)
+    {
+        for($i = $colS; $i < $y; ++$i)
+            $a[$rowS][$i] = $value++;
+            $rowS++;
+
+            for ($i = $rowS; $i < $x; ++$i)
+                $a[$i][$y - 1] = $value++;
+                $y--;
+
+                if ($rowS < $x)
+                {
+                    for($i = $y - 1;$i >= $colS; --$i)
+                        $a[$x - 1][$i] = $value++;
+                        $x--;
+                    }
+                    if($colS < $y){
+                        for($i = $x - 1; $i >= $rowS; --$i)
+                            $a[$i][$colS] = $value++;
+                        $colS++;
+                    }
+            }
+}
+?>
+
+
+
+</body>
+</html>
